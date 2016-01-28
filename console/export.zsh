574492d4c0ab24f7bab3736915b54eb99763cd39
Thomas Janhonen <thomas.janhonen@frantic.com>
2016-01-28 13:44:00 +0200

    implements docker support

--- console/export.zsh
+++ console/export.zsh
@@ -7,3 +7,5 @@ if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
 if which plenv > /dev/null; then eval "$(plenv init -)"; fi
 
 export PATH="/usr/local/heroku/bin:$PATH"
+
+eval $(docker-machine env default)
