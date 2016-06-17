7f29c9b581b01a96702cca9938b15def856701ae
Thomas Janhonen <thomas.janhonen@frantic.com>
2016-06-17 15:26:48 +0300

    android platform-tool

--- console/export.zsh
+++ console/export.zsh
@@ -7,3 +7,6 @@ if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
 if which plenv > /dev/null; then eval "$(plenv init -)"; fi
 
 export PATH="/usr/local/heroku/bin:$PATH"
+
+#android
+export PATH="$PATH:/Users/thomas.janhonen/Library/Android/sdk/platform-tools"
